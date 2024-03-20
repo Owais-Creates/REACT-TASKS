083a644f4577261b2b571352aebef61bbabdf04c
Owais <zaidalit00@gmail.com>
2024-03-20 17:47:04 +0530

    TASK5 - form handling using useRef

---
+++ task5/src/App.jsx
@@ -0,0 +1,28 @@
+// FORM SUBMISSION USING useREF
+import { useRef, useState } from 'react'
+
+function App() {
+
+  const name = useRef(null);
+  const email = useRef(null);
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log(` NAME - ${name.current.value} , EMAIL - ${email.current.value}`)
+  }
+
+  return (
+    <>
+
+      <form action="#" onSubmit={handleFormSubmission} >
+
+        <input ref={name} type="text" placeholder='name' />
+        <input ref={email} type="email" placeholder='email' />
+        <input type="submit" value={"Submit"} />
+
+      </form>
+      
+    </>
+  )
+}
+
+export default App
