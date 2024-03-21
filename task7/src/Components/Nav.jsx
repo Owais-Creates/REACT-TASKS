8c458e69dca2cee58b84591c6d1675f3adc3e3b6
Owais <zaidalit00@gmail.com>
2024-03-21 16:44:44 +0530

    used two approaches to make active highlighted links through router-DOM

--- task7/src/Components/Nav.jsx
+++ task7/src/Components/Nav.jsx
@@ -3,7 +3,27 @@ import { Link, NavLink } from "react-router-dom"
 const Nav = () => {
     return (
         <nav className="mt-10 flex justify-center gap-5" >
-            <NavLink to={"/users"} >Users</NavLink>
+
+            {/* Making active links using style in Router-DOM */}
+            <NavLink to={"/"} style={(e) => {
+                return {
+                    color: e.isActive ? "tomato" : "",
+                    fontWeight: e.isActive ? "bolder" : ""
+                }
+            }} >
+                Home
+            </NavLink>
+
+            {/* Making active links using className */}
+            <NavLink to={"/users"} className={(e) => {
+                return [
+                    e.isActive ? "text-red-600" : "",
+                    e.isActive ? "font-bold" : ""
+                ]
+            }} >
+                Users
+            </NavLink>
+
             <NavLink to={"/about"} >About</NavLink>
         </nav>
     )
