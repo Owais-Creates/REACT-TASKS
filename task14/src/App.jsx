c9c4d1ca6da4e8db57409611f114e1c230b1c700
Owais <zaidalit00@gmail.com>
2024-04-01 21:42:08 +0530

    addded todo create

---
+++ task14/src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { FaRegTrashAlt } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
+import { FaRegFaceSadCry } from "react-icons/fa6";
+
+
+import './App.css'; // Importing app.css file
+
+
+const App = () => {
+  const [todo, setTodo] = useState('');
+  const [todoData, setTodoData] = useState([]);
+
+  const addTask = () => {
+    if (todo.trim() === '') return;
+    setTodoData([...todoData, todo]);
+    setTodo('');
+  };
+
+  return (
+    <>
+      <div className='w-full h-screen bg-gray-400 flex justify-center'>
+        <div className='main-container w-[500px] h-[650px] p-9 bg-slate-100 mt-12'>
+          <h1 className='text-2xl text-center mb-10'>Todo List</h1>
+          <input
+            className='w-full p-3'
+            type='text'
+            placeholder='Here...'
+            value={todo}
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+          <button className='bg-red-200 p-3 mt-2' onClick={addTask}>
+            Add Task
+          </button>
+          <div className='mt-5 text-center text-2xl'>
+            <ul className='text-left font-mono '>
+              {todoData.length > 0 ? (
+                
+                  todoData.map((elem, index) => (
+
+                    <div className='flex justify-between items-center mb-8 '>
+                      <li className='todo-list-item text-[20px] bg-red-100 p-2 w-[350px] ' key={index}><p>{elem}</p></li>
+                      <div>
+                        <button className='text-[20px] '><FaRegTrashAlt /></button>
+                        <button className='text-[20px] ml-4 '><FaEdit /></button>
+                      </div>
+                    </div>
+
+                  ))
+                
+              ) : (
+                <h2 className='text-center font-bold ' >NO TASKS YET <FaRegFaceSadCry className='inline' /> </h2>
+              )}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
