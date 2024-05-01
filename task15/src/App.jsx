3f8b139455255d00e497d6ea15c1fff0b68b1f20
Owais <zaidalit00@gmail.com>
2024-05-01 21:04:54 +0530

    task15 ---> music

--- task15/src/App.jsx
+++ task15/src/App.jsx
@@ -6,60 +6,78 @@ import Counter from './Components/Counter'
 import Quiz from './Components/Quiz'
 import Select from './Components/Select'
 import AddFriends from './Components/AddFriends'
+import Music from './Components/Music'
 
 function App() {
 
-  const dummyData = [
-    {
-      name: "Owais",
-      profileImage: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      isFriend: false
-    },
-    {
-      name: "Raman",
-      profileImage: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      isFriend: false
-    },
-    {
-      name: "Savita",
-      profileImage: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      isFriend: false
-    }
-  ]
-
-  const [cardData, setCardData] = useState(dummyData);
-
-  const handleAddFriendButton = (itemIndex) => {
-
-    setCardData((prev) => (
-      prev.map((item, index) => {
-
-        if (index === itemIndex) { return { ...item, isFriend: !item.isFriend } }
-
-        return item;
-
-      })
-    ))
-
-  }
-
   return (
-
-    <>
-      <div className='flex w-full h-screen bg-zinc-800 justify-center items-center gap-6' >
-        {cardData.map((item, index) => (
-          <AddFriends
-            key={index}
-            values={item}
-            handleClick={handleAddFriendButton}
-            index={index}
-          />
-        ))}
-
-      </div>
-    </>
-
+    <div>
+      <Music />
+    </div>
   )
+
 }
 
+
+
 export default App
+
+
+
+
+
+
+//  <div>
+//    const dummyData = [
+//     {
+//       name: "Owais",
+//       profileImage: "https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       isFriend: false
+//     },
+//     {
+//       name: "Raman",
+//       profileImage: "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       isFriend: false
+//     },
+//     {
+//       name: "Savita",
+//       profileImage: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+//       isFriend: false
+//     }
+//   ]
+
+//   const [cardData, setCardData] = useState(dummyData);
+
+//   const handleAddFriendButton = (itemIndex) => {
+
+//     setCardData((prev) => (
+//       prev.map((item, index) => {
+
+//         if (index === itemIndex) { return { ...item, isFriend: !item.isFriend } }
+
+//         return item;
+
+//       })
+//     ))
+
+//   }
+
+//   return (
+
+//     <>
+//       <div className='flex w-full h-screen bg-zinc-800 justify-center items-center gap-6' >
+//         {cardData.map((item, index) => (
+//           <AddFriends
+//             key={index}
+//             values={item}
+//             handleClick={handleAddFriendButton}
+//             index={index}
+//           />
+//         ))}
+
+//       </div>
+//     </>
+
+//   )
+// }
+//  </div>
